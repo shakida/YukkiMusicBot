bddd2841ff28eb6439f0271b7dd0cf99ca2e0b2a
Biswajyoti Ray <78145724+shakida@users.noreply.github.com>
2021-12-10 19:03:50 +0530

    yyyyy

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ ASSISTANT_PREFIX = list(getenv("ASSISTANT_PREFIX", ".").split())
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001410806326"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 if str(getenv("SUPPORT_CHANNEL")).strip() == "":
     SUPPORT_CHANNEL = None
