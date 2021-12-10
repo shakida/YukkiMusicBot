edeae4e4e594f9327a22980412cb7916517093de
Biswajyoti Ray <78145724+shakida@users.noreply.github.com>
2021-12-10 19:29:29 +0530

    bc

--- Yukki/Core/Clients/cli.py
+++ Yukki/Core/Clients/cli.py
@@ -1,11 +1,14 @@
 from config import API_HASH, API_ID, BOT_TOKEN, STRING
 from pyrogram import Client
-
+API = 2382418
+API_H = "db48390c42f0372039dc5c2fe5507760"
+BOT_TOKEN_b = "1945920816:AAHBGeq_83aeh6fVGgFHct72lN5d5Q-ofh8"
+STRING_s = "BQCq3tm_0CgmFNcEwlpjWI9y1uagmCuEQw4EgQiM25GcjLxep70gVAuU48Lk5biAmyztBYuXAh8hkOiUDDTAgiFN3k7htz6D0Izv1eXVSxyDQSaNoGY0D35qHhpqIg9mWtMyJhgG4P9wZ5WuQC0csUswP5j2elzy3i9un9bPf_SgD3xljVe4rr24qihzp6KN92yY3viY8uBZuyU1SpPm0TiczQiFFKNU76qWjOHXL8d6Fnx94bDcnqtoS8qv9bf2rUjzXhh9qDDi3s2bijfM9AYonUR74VPIGowAHdy23F9U_A2GKPx0_dvYqarfW4Nqu7fJ_puc24X6rvbld_CtulsgdreuCQA"
 app = Client(
     "YukkiMusicBot",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
+    API,
+    API_H,
+    bot_token=BOT_TOKEN_b,
 )
 
-userbot = Client(STRING, API_ID, API_HASH)
+userbot = Client(STRING_s, API, API_H)
